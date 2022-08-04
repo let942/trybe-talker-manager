63417e1f24c7c9ca6388a2da491dd6e77615e5e3
soficia <let942@gmail.com>
2022-08-04 20:18:24 -0300

    fix: regex

--- login.js
+++ login.js
@@ -10,16 +10,14 @@ login.post('/login', (req, res) => {
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-
+  if (!(email.match(/^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/))) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
   if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-
-  if (!(email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i))) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
   return res.status(200).send({ token: generateToken() });
--- talker.js
+++ talker.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const fs = require('fs/promises');
+// const { nextTick } = require('process');
 
 const TALKERFILE = 'talker.json';
 
@@ -13,34 +14,6 @@ talker.get('/talker', async (req, res) => {
   const talkers = JSON.parse(fileTalker);
 
   return res.status(200).send(talkers);
-});
-
-function noReq(req, res, alert) {
-  if (!req) {
-    return res.status(401).json({ message: alert });
-  }
-
-}
-
-talker.post('/talker', (req, res) => {
-  const { name, age, talk } = req.body;
-  const { watchedAt, rate } = talk;
-  const token = req.headers.authorization;
-  noReq(token, res, 'Token não encontrado');
-  if (!token) {
-    return res.status(401).json({ message: 'Token não encontrado' });
-  }
-  if (token.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
-
-  if (!name) {
-    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  }
-  if (name.length < 3) {
-    return res.status(400).json({ message: 'O campo "name" deve ter pelo menos 3 caracteres' });
-  }
-
 });
 
 module.exports = { talker }; 
