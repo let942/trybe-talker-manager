cd16ea444ca4ceb3067d8d0e96233c4419016710
soficia <let942@gmail.com>
2022-08-04 07:59:19 -0300

    refactor: validação do endpoint /login

--- login.js
+++ login.js
@@ -5,12 +5,12 @@ const { generateToken } = require('./crypto');
 const login = express.Router();
 login.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  // const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  const regex = (/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i);
 
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!(email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/))) {
+  if (!(email.match(regex))) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) {
@@ -19,6 +19,6 @@ login.post('/login', async (req, res) => {
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
-  return res.status(200).json({ token: generateToken() });
+  return res.status(200).send({ token: generateToken() });
 });
 module.exports = { login };
