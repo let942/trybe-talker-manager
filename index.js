e94c922bb7e1d370b508ceaefe18ae63af4ec50b
soficia <let942@gmail.com>
2022-08-04 07:56:29 -0300

    feat: adiciona as validações para o endpoint /login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { talker } = require('./talker');
 const { talkerId } = require('./talkerId');
 const { login } = require('./login');
+// const { generateToken } = require('./crypto');
 
 const app = express();
 app.use(bodyParser.json());
--- login.js
+++ login.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const { generateToken } = require('./crypto');
 
-// const TALKERFILE = 'talker.json';
-
 const login = express.Router();
-login.post('/login', async (req, res) =>
-  res.status(200).send({ token: generateToken() }));
+login.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  // const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!(email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/))) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return res.status(200).json({ token: generateToken() });
+});
 module.exports = { login };
